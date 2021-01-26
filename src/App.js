a3d58b4bd9b91d98840cbec7efecef2078e876c0
Matthias Enzi <matthias@upleveled.io>
2021-01-26 21:24:17 +0100

    Add comments in code.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+
+// JS Library to get colors.
 const randomColor = require('randomcolor');
 
+// Component and state declaration.
 export default function ColorGenerator() {
   const [color, setColor] = useState(randomColor);
 
+  // Return Text, Color Button and Hex Code.
   return (
     <div>
       <h1>RANDOM COLOR GENERATOR</h1>
