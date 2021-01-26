6326ddaca8ed19095dea373a0cd9bd06d865cc0b
Matthias Enzi <matthias@upleveled.io>
2021-01-26 21:19:43 +0100

    Add random color generator and HEX code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+const randomColor = require('randomcolor');
 
-function App() {
+export default function ColorGenerator() {
+  const [color, setColor] = useState(randomColor);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>RANDOM COLOR GENERATOR</h1>
+
+      <button
+        onClick={() => {
+          setColor(randomColor);
+        }}
+      >
+        NEW COLOR
+      </button>
+
+      <h2 style={{ color: color }}>RANDOM COLOR = {color}</h2>
     </div>
   );
 }
-
-export default App;
